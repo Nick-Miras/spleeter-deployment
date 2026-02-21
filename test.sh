42372201854feb946c51031f0dfaa1c32305a4b1
Nick Anthony Miras <mirasnickanthony@gmail.com>
2026-02-21 18:05:57 +0800

    Remove hardcoded paths from config and update test script to fix dir path

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 mkdir output_directory
 
-python -m splitter separate -o output_directory/ -p configs/modified_config.json audiotest/testmusic.mp3
+python -m spleeter separate -o output_directory/ -p configs/modified_config.json audiotest/testmusic.mp3
+
+zip -r results.zip output_directory/
