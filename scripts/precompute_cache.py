6b67b8cfef8a0610a17865bcb8439f2e62120966
Nick Anthony Miras <mirasnickanthony@gmail.com>
2026-02-25 21:42:51 +0800

    Refactor precompute cache script to optimize dataset pipeline and improve caching process

--- scripts/precompute_cache.py
+++ scripts/precompute_cache.py
@@ -3,27 +3,31 @@ from spleeter.utils.configuration import load_configuration
 from spleeter.dataset import DatasetBuilder
 from spleeter.audio.adapter import AudioAdapter
 
-# 1. Load your configuration
+
 config = load_configuration("configs/modified_config.json")
 audio_adapter = AudioAdapter.default()
 
-# 2. Initialize the Dataset Builder
-# Note: Adjust 'audio_path' to point to your root dataset directory
+
 builder = DatasetBuilder(
     config, 
     audio_adapter, 
-    audio_path="data/Datasets"
+    audio_path="data"
 )
 
-# 3. Build the dataset pipeline
+
 train_dataset = builder.build(
     config["train_csv"], 
-    cache_directory=config["training_cache"]
+    cache_directory=config["training_cache"],
+    batch_size=128
 )
 
-# 4. Iterate to force the CPU to compute STFTs and write the cache
+
+ # prefetch
+optimized_dataset = train_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+        
+        
 print("Caching training data... This will maximize your CPU usage.")
-for i, batch in enumerate(train_dataset):
+for i, batch in enumerate(optimized_dataset):
     if i % 50 == 0:
         print(f"Processed {i} chunks...")
         
