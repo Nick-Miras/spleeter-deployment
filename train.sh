5c981a4237b7e7c8e98f70ebf7775580a1a03791
Nick Anthony Miras <mirasnickanthony@gmail.com>
2026-02-27 18:00:36 +0800

    changed name of archived zip file

--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@ export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 python -m spleeter train -p configs/modified_config.json -d data/
 
-zip -r training_output.zip training_output/
+zip -r model_weights.zip spleeter_pretrained1/
