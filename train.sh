fae824699b252e5569c58b05a05d5a3d961f1ba5
Nick Anthony Miras <mirasnickanthony@gmail.com>
2026-02-27 21:53:50 +0800

    added date suffix to model_weights saving

--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@ export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 python -m spleeter train -p configs/modified_config.json -d data/
 
-zip -r model_weights.zip spleeter_pretrained1/
+zip -r model_weights_$(date +%Y%m%d_%H%M).zip spleeter_pretrained1/
