5248e86fc5cad0f2865a87e5ed5b19b5c1a15fdb
Nick Anthony Miras <mirasnickanthony@gmail.com>
2026-03-01 18:47:02 +0800

    modified user script

--- deployment/training_instance/main.tf
+++ deployment/training_instance/main.tf
@@ -1,26 +1,26 @@
 
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
-  volume_id   = "vol-06024cfa4266ffe34"
+  volume_id   = "vol-025382c6241f97fcb"  # TODO: Use Variables
   instance_id = "${aws_instance.spleeter_training_instance.id}"
   # skip_destroy = true
 }
 
 resource "aws_instance" "spleeter_training_instance" {
   ami           = "ami-02d2c097a1e2910f5"  # Nvidia Base CUDA AMI
-  instance_type = "g6.xlarge"
+  instance_type = "g4dn.xlarge"
   key_name      = "spleeter-training"
   availability_zone = "us-east-1d"
   vpc_security_group_ids = ["sg-05d98613cfd942528", "sg-0b5f5c58a5fd8d76f"]
 
   root_block_device {
-    volume_size = 150
+    volume_size = 50
     volume_type = "gp3"
     throughput = 125
     iops = 3000
   }
   tags = {
-    Name = "Spleeter Training g6 Instance"
+    Name = "Spleeter Training g4dn Instance"
   }
 
   user_data = <<-EOF
@@ -29,10 +29,7 @@ resource "aws_instance" "spleeter_training_instance" {
 
       # Install necessary packages
       apt-get install ffmpeg zip -y
-      # add-apt-repository ppa:deadsnakes/ppa
-      # apt update
-      # apt install python3.10 python3.10-venv -y
-    
+
       # Wait for the device to be attached
       while [ ! -e /dev/nvme1n1 ]; do sleep 1; done
       mkdir -p /mnt/thesis
@@ -58,22 +55,28 @@ resource "aws_instance" "spleeter_training_instance" {
         
         touch installation_log.txt
         
-        # --- Fix for EXTERNALLY MANAGED ENVIRONMENT ---
-        # You must create the venv AND activate it within this block.
-        # When the venv is active, pip checks get bypassed.
-        ~/miniconda3/bin/conda config --set always_yes true
+        ##########################################################
+        # --- Conda Managed Environment for cudnn dependencies ---
+        ##########################################################
+
+        # Accept licenses for Anaconda packages
+        ~/miniconda3/bin/conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+        ~/miniconda3/bin/conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
 
         # echo "Creating virtual environment..."
-        ~/miniconda3/bin/conda env create -f environment.yaml >> installation_log.txt 2>&1
+        ~/miniconda3/bin/conda env create -f environment.yml >> installation_log.txt 2>&1
 
         # Initialize Conda
         ~/miniconda3/bin/conda init
 
         # Make the new environment activate automatically on SSH login
-        echo "conda activate spleeter" >> $HOME_DIR/.bashrc
+        echo "conda activate spleeter" >> ~/.bashrc
 
-        # Finalizations
-        chmod +x setup.sh train.sh
+        # Setting Permissions
+        chmod +x setup.sh train.sh test.sh
+
+        # Copying Cache to the new environment
+        cp -r /mnt/thesis/spleeter-deployment/cache/* cache/
       EOU
     EOF
 }
