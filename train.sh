6fc70a2a15383a1387904a6fb55c018c28e824ed
Nick Anthony Miras <mirasnickanthony@gmail.com>
2026-03-03 10:57:22 +0800

    modified cp command

--- train.sh
+++ train.sh
@@ -5,6 +5,6 @@ python -m spleeter train -p configs/modified_config.json -d data/
 FILE_NAME=model_weights_$(date +%Y%m%d_%H%M)
 zip -r "$FILE_NAME.zip" spleeter_pretrained1/
 
-cp "$FILE_NAME" /mnt/thesis/spleeter-deployment/
+cp "$FILE_NAME.zip" /mnt/thesis/spleeter-deployment/
 
 # sudo shutdown -h +1
