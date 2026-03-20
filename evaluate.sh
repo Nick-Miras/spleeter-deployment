bb2f8f1b291b514926a19c5ea74924da75a77f90
Nick Anthony Miras <mirasnickanthony@gmail.com>
2026-03-20 12:26:44 +0800

    Update .gitignore and enhance evaluation scripts; add missing dependencies to environment files

--- evaluate.sh
+++ evaluate.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-wget https://zenodo.org/records/1117372/files/musdb18.zip
-unzip musdb18.zip -d data/
+
+wget https://zenodo.org/records/1117372/files/musdb18.zip?download=1 -O musdb18.zip
+mkdir data/musdb18
+unzip musdb18.zip -d data/musdb18/
 rm musdb18.zip
 
 hf download NickAnthonyMiras/spleeter-thesis-1.514m --repo-type model --local-dir spleeter_pretrained1/
 
-python scripts/custom_evaluate.py --config configs/modified_config.json --dataset_dir data
+python create_paths.py
+python scripts/evaluate.py \
+    --model configs/modified_config.json \
+    --out metrics.csv \
+    --stem-names drums bass other vocals \
+    --dataset data/musdb18/test/ \
+    --stem-mp4-batch
