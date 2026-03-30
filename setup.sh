7a7feb2e8cc679614f61183fe96d0078cc7a0a37
Nick Anthony Miras <mirasnickanthony@gmail.com>
2026-03-30 17:15:04 +0800

    Update setup script; add precomputed cache download and replace model download method

--- setup.sh
+++ setup.sh
@@ -9,11 +9,16 @@ aws s3 sync s3://spleeter-dataset-338310096867-us-east-1-an data/Datasets
 cd "$PROJECT_ROOT"
 python3 setup_config.py
 
+# Download precomputed cache
+echo "Downloading precomputed cache..."
+aws s3 sync s3://spleeter-cache-338310096867-us-east-1-an cache/
 
 # Download pretrained model
 echo "Downloading pretrained model..."
 mkdir -p spleeter_pretrained1
 cd spleeter_pretrained1
-hf download NickAnthonyMiras/spleeter-thesis-1.514m --repo-type model --local-dir .
+wget https://github.com/deezer/spleeter/releases/download/v1.4.0/4stems-finetune.tar.gz
+tar -xzf 4stems-finetune.tar.gz
+rm 4stems-finetune.tar.gz
 
 echo "Setup complete. You can now run 'train.sh' to start training and 'monitor.sh' to monitor with TensorBoard."
